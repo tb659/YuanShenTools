71d873417666974912ea07b44e2bcf4a108ecd7b
tb659 <474779508@qq.com>
2026-07-28 02:29:25 +0800

    拖拽和透明

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace YuanShenTools
 {
@@ -39,6 +41,12 @@ namespace YuanShenTools
             await WebView.EnsureCoreWebView2Async();
             WebView.DefaultBackgroundColor = System.Drawing.Color.Transparent;
 
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            var style = GetWindowLong(hwnd, GWL_STYLE);
+            style |= WS_CAPTION;
+            SetWindowLong(hwnd, GWL_STYLE, style);
+            SetWindowPos(hwnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+
             var cfg = Config.Load();
             Config.ApplyWindow(this, cfg);
             Opacity = cfg.Opacity;
@@ -66,6 +74,22 @@ namespace YuanShenTools
             var cfg = Config.FromWindow(this, UrlTextBox.Text, Opacity);
             Config.Save(cfg);
         }
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private const int GWL_STYLE = -16;
+        private const int WS_CAPTION = 0x00C00000;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOZORDER = 0x0004;
+        private const uint SWP_FRAMECHANGED = 0x0020;
 
         private void RegisterHotkeys()
         {
@@ -177,9 +201,155 @@ document.head.appendChild(s);
                 await WebView.ExecuteScriptAsync(script);
         }
 
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetCapture(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        private const int VK_LBUTTON = 0x01;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT { public int X; public int Y; }
+
+        private bool _isDragging;
+        private string? _resizeEdge;
+        private bool _isResizing;
+        private double _dragStartLeft, _dragStartTop;
+        private double _resizeStartLeft, _resizeStartTop, _resizeStartWidth, _resizeStartHeight;
+        private System.Windows.Threading.DispatcherTimer? _dragTimer;
+        private const double MIN_WIDTH = 200;
+        private const double MIN_HEIGHT = 100;
+
         private void DragBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left) DragMove();
+            if (e.ChangedButton != MouseButton.Left) return;
+            _isDragging = true;
+            _dragStartLeft = Left;
+            _dragStartTop = Top;
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetCapture(hwnd);
+            GetCursorPos(out var start);
+            var startX = start.X; var startY = start.Y;
+
+            _dragTimer = new System.Windows.Threading.DispatcherTimer();
+            _dragTimer.Interval = TimeSpan.FromMilliseconds(10);
+            _dragTimer.Tick += (s, args) =>
+            {
+                if (!_isDragging) { _dragTimer?.Stop(); return; }
+                if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000) == 0)
+                {
+                    _isDragging = false;
+                    ReleaseCapture();
+                    _dragTimer?.Stop();
+                    return;
+                }
+                GetCursorPos(out var cur);
+                var dx = cur.X - startX;
+                var dy = cur.Y - startY;
+                SetWindowPos(hwnd, IntPtr.Zero,
+                    (int)(_dragStartLeft + dx), (int)(_dragStartTop + dy),
+                    0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            };
+            _dragTimer.Start();
+        }
+
+        private void ResizeMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left) return;
+            _resizeEdge = ((FrameworkElement)sender).Name;
+            _resizeStartLeft = Left;
+            _resizeStartTop = Top;
+            _resizeStartWidth = Width;
+            _resizeStartHeight = Height;
+            _isResizing = true;
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetCapture(hwnd);
+            GetCursorPos(out var start);
+            var startX = start.X; var startY = start.Y;
+
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += (s, args) =>
+            {
+                if (!_isResizing) { timer?.Stop(); return; }
+                if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000) == 0)
+                {
+                    _isResizing = false;
+                    ReleaseCapture();
+                    timer?.Stop();
+                    return;
+                }
+                GetCursorPos(out var cur);
+                UpdateResize(cur.X, cur.Y);
+            };
+            timer.Start();
+        }
+
+        private void UpdateResize(int cursorX, int cursorY)
+        {
+            double newLeft = Left, newTop = Top, newWidth = Width, newHeight = Height;
+            var origRight = _resizeStartLeft + _resizeStartWidth;
+            var origBottom = _resizeStartTop + _resizeStartHeight;
+
+            switch (_resizeEdge)
+            {
+                case "ResizeLeft":
+                    newWidth = origRight - cursorX;
+                    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+                    newLeft = origRight - newWidth;
+                    break;
+                case "ResizeRight":
+                    newWidth = cursorX - Left;
+                    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+                    break;
+                case "ResizeTop":
+                    newHeight = origBottom - cursorY;
+                    if (newHeight < MIN_HEIGHT) newHeight = MIN_HEIGHT;
+                    newTop = origBottom - newHeight;
+                    break;
+                case "ResizeBottom":
+                    newHeight = cursorY - Top;
+                    if (newHeight < MIN_HEIGHT) newHeight = MIN_HEIGHT;
+                    break;
+                case "ResizeTopLeft":
+                    newWidth = origRight - cursorX;
+                    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+                    newLeft = origRight - newWidth;
+                    newHeight = origBottom - cursorY;
+                    if (newHeight < MIN_HEIGHT) newHeight = MIN_HEIGHT;
+                    newTop = origBottom - newHeight;
+                    break;
+                case "ResizeTopRight":
+                    newWidth = cursorX - Left;
+                    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+                    newHeight = origBottom - cursorY;
+                    if (newHeight < MIN_HEIGHT) newHeight = MIN_HEIGHT;
+                    newTop = origBottom - newHeight;
+                    break;
+                case "ResizeBottomLeft":
+                    newWidth = origRight - cursorX;
+                    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+                    newLeft = origRight - newWidth;
+                    newHeight = cursorY - Top;
+                    if (newHeight < MIN_HEIGHT) newHeight = MIN_HEIGHT;
+                    break;
+                case "ResizeBottomRight":
+                    newWidth = cursorX - Left;
+                    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+                    newHeight = cursorY - Top;
+                    if (newHeight < MIN_HEIGHT) newHeight = MIN_HEIGHT;
+                    break;
+            }
+            SetWindowPos(new WindowInteropHelper(this).Handle, IntPtr.Zero,
+                (int)newLeft, (int)newTop, (int)newWidth, (int)newHeight,
+                SWP_NOZORDER);
         }
 
         private void GoButton_Click(object sender, RoutedEventArgs e) => NavigateToUrl();
